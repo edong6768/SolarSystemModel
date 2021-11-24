ceda7a2b923e17f5943a41be7f826b953dbb133b
edong6768 <edong6768@gmail.com>
2021-11-24 17:29:23 +0900

    add additional SSM IO methods

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import ssm.celestials as cel
 
-cel.solar_system_model.config_JSON_load('data/SEM_config_snapshot_0.json')
+a=cel.solar_system_model.config_JSON_load('data/SEM_config_snapshot_0.json')
+
+a.config_obj_write("data/SEM_model.joblib")
+b=cel.solar_system_model.config_obj_load("data/SEM_model.joblib")
+print(b)
