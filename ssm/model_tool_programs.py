9a90aa651debe63d2f15fb02eec7204a32323f8c
edong6768 <edong6768@gmail.com>
2021-12-14 18:40:01 +0900

    FEAT tool improved

--- ssm/model_tool_programs.py
+++ ssm/model_tool_programs.py
@@ -1,5 +1,18 @@
 import celestials as cel
 import matplotlib.pyplot as plt
+
+def console_interface(title, tools):
+    print("\n----------< %s >----------"%(title))
+    for i, modes in enumerate(tools): print("--%d. %s"%(i+1, modes[0]))
+    print("-"*(len(title)+24))
+
+    while(True):
+        mode=int(input(" Select mode : "))
+        print("\n==> %d. %s selected"%(i+1, modes[0]))
+        if mode not in list(range(len(tools))): break
+        title, tool, fargs=tools[mode-1]
+        tool(*fargs)
+        print("Program terminated.")
     
 def load_model():
     model=cel.solar_system_model.config_JSON_load('data/random_ssm_config_snapshot_0.json')
@@ -41,7 +54,7 @@ def animation_tool(model):
         # 현 관측자 위도/경도에서 평먄에 투영한 천구상 천체들의 움직임
         print("5. 2d local celestial sphere animation mode selected")
         ax2=fig.add_subplot()
-        model.local_cheongu_animation2d(fig, ax2, 126, 37, 60, )
+        model.local_cheongu_animation2d(fig, ax2, 126, 37, 600, )
     else : print("Wrong number!")
 
     print("Program terminated")
@@ -51,19 +64,19 @@ def plot_tools(model):
     # 항성계의 각 천체와 공전 궤도 plot
     fig1=plt.figure()
     ax1=fig1.add_subplot(projection='3d')
-    model.plot(ax1, frame = 0, sphere = False)
+    model.plot(ax1, frame = 0, sphere = True)
 
     # 관측자 천체의 북반구에서의 천구 현상 plot
-    fig2=plt.figure()
-    ax2=fig2.add_subplot(projection='3d')
+    # fig2=plt.figure()
+    # ax2=fig2.add_subplot(projection='3d')
 
-    fig3=plt.figure()
-    ax3=fig3.add_subplot()
-
-    for i in range(5):
-        model.timetravel_diff(M=30)
-        model.cheongu_plot(ax2)
-        model.cheongu_plot2d(ax3, 0)
+    # fig3=plt.figure()
+    # ax3=fig3.add_subplot()
+    
+    # for i in range(5):
+    #     model.timetravel_diff(M=30)
+    #     model.cheongu_plot(ax2)
+    #     model.cheongu_plot2d(ax3, 0)
 
     # 각 plot을 보기 위해서 show
     plt.show()
@@ -89,18 +102,9 @@ def eclipse_search_tool(model):
 
 
 model=load_model()
-print("""-----------< Tool selection >-----------
---1. orbit animation
---2. celestial sphere animation
---3. 2d celestial sphere animation
---4. local celectial sphere animation
---5. 2d local celestial sphere animation
---6. Quit
------------------------------------------""")
 
-tools=[animation_tool, plot_tools, eclipse_search_tool]
-while(True):
-    modes=int(input(" Select mode : "))
-    if modes not in list(range(6)): break
-    tools[modes-1](model)
-print("Program terminated.")
+tools=[("animation_tool", animation_tool, (model,)), 
+        ("plot tools", plot_tools, (model,)),
+        ("eclipse search tool", eclipse_search_tool, (model,))]
+        
+console_interface('Tool selection', tools)
